99f2a0e49ff250d36dfa358d428b49ae1b97a8d7
Daniel Wisdom <2019dwisdom@ike.csl.tjhsst.edu>
2018-10-20 17:29:10 -0400

    work

--- core.cpp
+++ core.cpp
@@ -7,17 +7,94 @@ struct Edge {
 	int d;
 	int time;
 	int dist;
-} typedef Edge;
+};
 
 struct Node {
 	int id;
 	vector<Edge> adj;
 	int x;
 	int y;
-} typedef Node;
+};
 
 Node* gr=0;
 int nodes;
+
+struct QE {
+	void* val;
+	long long t;
+};
+
+struct PQ {
+	QE* a=0;
+	int sz=0;
+	int maxsz=-1;
+};
+
+void deletePQ(PQ p) {
+	free(p.a);
+	p.sz=0;
+	p.maxsz=-1;
+}
+
+void initPQ(PQ p, int maxnum) {
+	deletePQ(p);
+	p.a=(QE*)malloc(sizeof(QE)*maxnum+1);
+	p.maxsz=maxnum;
+}
+
+void swap(QE* a, int i, int k) {
+	void* temp1=a[i].val;
+	long long temp2=a[i].t;
+	a[i].val=a[k].val;
+	a[i].t=a[k].t;
+	a[k].val=temp1;
+	a[k].t=temp2;
+}
+
+void heapUp(PQ p, int i) {
+	while (i>1) {
+		if (p.a[i].t<p.a[i/2].t) {
+			swap(p.a,i,i/2);
+			i/=2;
+		} else {
+			return;
+		}
+	}
+}
+
+void heapDown(PQ p, int i) {
+	while (i*2<=p.sz) {
+		if (i*2+1<=p.sz && p.a[i*2+1].t<p.a[i].t && p.a[i*2+1].t<=p.a[i*2].t) {
+			p.a[i]=p.a[i*2+1];
+			i=i*2+1;
+		} else if (p.a[i*2].t<p.a[i].t) {
+			p.a[i]=p.a[i*2];
+			i*=2;
+		} else {
+			return;
+		}
+	}
+}
+
+void push(PQ p, long long t, void* v) {
+	if (p.sz==p.maxsz) {
+		cout<<"Error too many PQ elements\n";
+		return;
+	}
+	p.a[p.sz+1].t=t;
+	p.a[p.sz+1].val=v;
+	p.sz++;
+	heapUp(p,p.sz+2);
+}
+
+QE pop(PQ p) {
+	if (p.sz==0) {
+		cout<<"Nothing in the pq\n";
+	}
+	QE ret=p.a[1];
+	heapDown(p,1);
+	return ret;
+}
 
 void readCoords(FILE* f) {
 	char line[256];
